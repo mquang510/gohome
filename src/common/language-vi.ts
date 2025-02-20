c0dd645cd48c5b63999439045938ff409b026e30
qupham <qupham@deloitte.ca>
2025-02-20 15:24:48 +0700

    add

---
+++ src/common/language-vi.ts
@@ -0,0 +1,13 @@
+export const informationText = {
+    header: "Chào!",
+    body: "Trò chơi tìm đường về nhà.",
+    instruction: "Sử dụng phím lên, xuống, phải, trái để di chuyển.",
+    startButton: "Bắt đầu"
+}
+
+export const resultText = {
+    header: "Chào!",
+    body: "Chào mừng bạn về tới nhà, đi ngủ nào.",
+    information: "Tổng số bước đã di chuyển: ",
+    startButton: "Chơi lại"
+}
---
+++ src/component/popup/result.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { resultText } from '../../common/language-vi.ts'
+
+export default function ResultPopup({ isCompleted, onHide, total }) {
+  return (
+    <>
+      <Modal show={isCompleted} centered={true}>
+        <Modal.Header>
+          <Modal.Title>{resultText.header}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='mb-3'>
+            {resultText.body}
+          </div>
+          <div className='mb-3'>
+            {resultText.information} {total}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={onHide}>
+            {resultText.startButton}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
